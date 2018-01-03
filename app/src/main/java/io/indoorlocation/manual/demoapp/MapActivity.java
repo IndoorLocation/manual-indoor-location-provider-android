4452968436a8e061490cdf8a85548387ab0b7c78
Etienne Mercier <etienne@mapwize.io>
2018-01-03 15:54:40 +0100

    Initial commit

---
+++ app/src/main/java/io/indoorlocation/manual/demoapp/MapActivity.java
@@ -0,0 +1,118 @@
+package io.indoorlocation.manual.demoapp;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+import io.indoorlocation.core.IndoorLocation;
+import io.indoorlocation.manual.ManualIndoorLocationProvider;
+import io.mapwize.mapwizeformapbox.MapOptions;
+import io.mapwize.mapwizeformapbox.MapwizePlugin;
+import io.mapwize.mapwizeformapbox.model.LatLngFloor;
+
+public class MapActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    private MapwizePlugin mapwizePlugin;
+    private ManualIndoorLocationProvider manualIndoorLocationProvider;
+    private static final int MY_PERMISSION_ACCESS_FINE_LOCATION = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, "pk.eyJ1IjoibWFwd2l6ZSIsImEiOiJjamNhYnN6MjAwNW5pMnZvMnYzYTFpcWVxIn0.veTCqUipGXCw8NwM2ep1Xg");// PASTE YOU MAPBOX API KEY HERE !!! This is a demo key. It is not allowed to use it for production. The key might change at any time without notice. Get your key by signing up at mapbox.com
+        setContentView(R.layout.activity_map);
+
+        mapView = findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                mapwizePlugin = new MapwizePlugin(mapView, mapboxMap, new MapOptions());
+                setupLocationProvider();
+
+                mapwizePlugin.setOnMapClickListener(new MapwizePlugin.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(LatLngFloor latLngFloor) {
+                        Location location = new Location(manualIndoorLocationProvider.getName());
+                        location.setLatitude(latLngFloor.getLatitude());
+                        location.setLongitude(latLngFloor.getLongitude());
+                        IndoorLocation indoorLocation = new IndoorLocation(manualIndoorLocationProvider.getName(), latLngFloor.getLatitude(), latLngFloor.getLongitude(), latLngFloor.getFloor(), System.currentTimeMillis());
+                        manualIndoorLocationProvider.setIndoorLocation(indoorLocation);
+                    }
+                });
+            }
+        });
+    }
+
+    private void setupLocationProvider() {
+        manualIndoorLocationProvider = new ManualIndoorLocationProvider();
+        mapwizePlugin.setLocationProvider(manualIndoorLocationProvider);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_ACCESS_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    setupLocationProvider();
+
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+}
